a48ece751c1e864a56f0936a79b8e348e1311d4c
Vasyl Kalymin <38068Vasia@gmail.com>
2023-05-07 13:45:43 +0300

    finished my seventh homework

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,48 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryLIst = document.querySelector(".gallery");
+const renderList = createGalleryItems(galleryItems);
+galleryLIst.insertAdjacentHTML("afterbegin", renderList);
+galleryLIst.addEventListener("click", openOriginalPfoto);
 
-console.log(galleryItems);
+function createGalleryItems(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+    })
+    .join("");
+}
+
+const instance = basicLightbox.create(
+  `<img width="1280" height="auto" src="">`,
+  {
+    onShow: (instance) => {
+      window.addEventListener("keydown", onEscKeyPress);
+    },
+    onClose: (instance) => {
+      window.removeEventListener("keydown", onEscKeyPress);
+    },
+  }
+);
+
+function openOriginalPfoto(e) {
+  e.preventDefault();
+  const datasetSource = e.target.dataset.source;
+  if (!datasetSource) return;
+  instance.element().querySelector("img").src = datasetSource;
+  instance.show();
+}
+
+function onEscKeyPress(e) {
+  if (e.code !== "Escape") return;
+  instance.close();
+}
